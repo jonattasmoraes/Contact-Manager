c972d3fc83b5538cf6f011d0dc5a4e890447890b
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2023-11-12 20:58:42 -0300

    feat: add useState to Contact component, implement ternary logic for buttons, and componentize project buttons

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,29 +1,49 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Contact = () => (
-  <S.ContactCard>
-    <S.MobileTitleContainer>
-      <S.ContactTitle>Nome do contato</S.ContactTitle>
-      <S.ContactTitle>E-mail</S.ContactTitle>
-      <S.ContactTitlePhone>Telefone</S.ContactTitlePhone>
-    </S.MobileTitleContainer>
-    <S.DataContainer>
-      <S.ContactTitleMobile>Nome do contato</S.ContactTitleMobile>
-      <S.ContactData>Jônattas Moraes</S.ContactData>
-      <S.ContactTitleMobile>E-mail</S.ContactTitleMobile>
-      <S.ContactData>jonattasmoraes@hotmail.com</S.ContactData>
-      <S.ContactTitleMobile>Telefone</S.ContactTitleMobile>
-      <S.ContactDataPhone>41 99999-9999</S.ContactDataPhone>
-    </S.DataContainer>
-    <S.ContactTag>Família</S.ContactTag>
-    <S.ContactTag>Trabalho</S.ContactTag>
-    <S.ContactTag>Emergencia</S.ContactTag>
-    <S.ContactTag>Bloqueado</S.ContactTag>
-    <S.ActionBar>
-      <S.Button>Editar</S.Button>
-      <S.Button>Deletar</S.Button>
-    </S.ActionBar>
-  </S.ContactCard>
-)
+type Props = {
+  name: string
+  email: string
+  phone: string
+  type: string
+  state: string
+}
+
+const Contact = ({ name, email, phone, type, state }: Props) => {
+  const [itsEditing, setItsEditing] = useState(false)
+
+  return (
+    <S.ContactCard>
+      <S.MobileTitleContainer>
+        <S.ContactTitle>Nome do contato</S.ContactTitle>
+        <S.ContactTitle>E-mail</S.ContactTitle>
+        <S.ContactTitlePhone>Telefone</S.ContactTitlePhone>
+      </S.MobileTitleContainer>
+      <S.DataContainer>
+        <S.ContactTitleMobile>Nome do contato</S.ContactTitleMobile>
+        <S.ContactData value={name} />
+        <S.ContactTitleMobile>E-mail</S.ContactTitleMobile>
+        <S.ContactData value={email} />
+        <S.ContactTitleMobile>Telefone</S.ContactTitleMobile>
+        <S.ContactDataPhone value={phone} />
+      </S.DataContainer>
+      <S.ContactTag>{type}</S.ContactTag>
+      <S.ContactTag>{state}</S.ContactTag>
+      <S.ActionBar>
+        {itsEditing ? (
+          <>
+            <S.Button>Salvar</S.Button>
+            <S.Button onClick={() => setItsEditing(true)}>Cancelar</S.Button>
+          </>
+        ) : (
+          <>
+            <S.Button onClick={() => setItsEditing(true)}>Editar</S.Button>
+            <S.Button>Deletar</S.Button>
+          </>
+        )}
+      </S.ActionBar>
+    </S.ContactCard>
+  )
+}
 
 export default Contact
--- src/containers/contactList/index.tsx
+++ src/containers/contactList/index.tsx
@@ -1,25 +1,59 @@
 import Contact from '../../components/contact'
 import { Container } from './styles'
 
+const contacts = [
+  {
+    name: 'Jônattas Moraes',
+    email: 'jonatas.moraes.09@hotmail.com',
+    phone: '(41) 99999-9999',
+    type: 'Família',
+    state: 'Ativo'
+  },
+  {
+    name: 'Ana Julia',
+    email: 'anajulia@hotmail.com',
+    phone: '(41) 99999-9999',
+    type: 'Amigos',
+    state: 'Ativo'
+  },
+  {
+    name: 'Tiago Teixeira',
+    email: 'tiago100@hotmail.com',
+    phone: '(41) 99999-9999',
+    type: 'Família',
+    state: 'Ativo'
+  },
+  {
+    name: 'Silvana teles',
+    email: 'silvateles@hotmail.com',
+    phone: '(41) 99999-9999',
+    type: 'Trabalho',
+    state: 'Bloqueado'
+  },
+  {
+    name: 'Policia Militar',
+    email: 'policia@gov.pr',
+    phone: '190',
+    type: 'Emergência',
+    state: 'Ativo'
+  }
+]
+
 const ContactList = () => (
   <Container>
-    <p>cinco conteudos como macados para amanha</p>
+    <p>cinco contatos marcados para amanhã</p>
     <ul>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
-      <li>
-        <Contact />
-      </li>
+      {contacts.map((c) => (
+        <li key={c.name}>
+          <Contact
+            name={c.name}
+            email={c.email}
+            phone={c.phone}
+            type={c.type}
+            state={c.state}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
