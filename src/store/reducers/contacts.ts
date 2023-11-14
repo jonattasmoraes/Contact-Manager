349e310064f2270a18f6da5fe145e8ac3c109d22
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2023-11-14 20:09:55 -0300

    feat: created reducer for editing contacts

--- src/store/reducers/contacts.ts
+++ src/store/reducers/contacts.ts
@@ -2,89 +2,107 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import Contact from '../../models/Contact'
 import * as enums from '../../utils/enums/Contact'
 
+type contactState = {
+  items: Contact[]
+}
+
+const initialState: contactState = {
+  items: [
+    {
+      name: 'Ana Beatriz',
+      email: 'anaB@hotmail.com',
+      phone: '(11) 11111-1111',
+      type: enums.Type.TRABALHO,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.CELULAR,
+      id: 1
+    },
+    {
+      name: 'Ruan Carlos',
+      email: 'ruan166@gmail.com',
+      phone: '(55) 99999-9999',
+      type: enums.Type.TRABALHO,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.CELULAR,
+      id: 4
+    },
+    {
+      name: 'Policia Militar',
+      email: 'policia@gov.br',
+      phone: '190',
+      type: enums.Type.EMERGENCIA,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.FIXO,
+      id: 5
+    },
+    {
+      name: 'Marcos Paulo',
+      email: 'marcospaulo@hotmail.com',
+      phone: '(41) 99999-9999',
+      type: enums.Type.AMIGOS,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.CELULAR,
+      id: 6
+    },
+    {
+      name: 'Jônattas Moraes',
+      email: 'jonatas.moraes.09@hotmail.com',
+      phone: '(41) 99999-9999',
+      type: enums.Type.FAMILIA,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.CELULAR,
+      id: 7
+    },
+    {
+      name: 'Ana Julia',
+      email: 'anajulia@hotmail.com',
+      phone: '(41) 99999-9999',
+      type: enums.Type.AMIGOS,
+      status: enums.Status.BLOQUEADO,
+      phoneType: enums.PhoneType.CELULAR,
+      id: 8
+    },
+    {
+      name: 'Tiago Teixeira',
+      email: 'tiago100@hotmail.com',
+      phone: '(41) 3999-9999',
+      type: enums.Type.FAMILIA,
+      status: enums.Status.ATIVO,
+      phoneType: enums.PhoneType.FIXO,
+      id: 9
+    },
+    {
+      name: 'Silvana teles',
+      email: 'silvateles@hotmail.com',
+      phone: '(41) 3699-9999',
+      type: enums.Type.TRABALHO,
+      status: enums.Status.BLOQUEADO,
+      phoneType: enums.PhoneType.FIXO,
+      id: 10
+    }
+  ]
+}
+
 const contactSlice = createSlice({
   name: 'contacts',
-  initialState: [
-    new Contact(
-      'Ana Beatriz',
-      'anaB@hotmail.com',
-      '(11) 11111-1111',
-      enums.Type.TRABALHO,
-      enums.Status.ATIVO,
-      enums.PhoneType.CELULAR,
-      1
-    ),
-    new Contact(
-      'Ruan Carlos',
-      'ruan166@gmail.com',
-      '(55) 99999-9999',
-      enums.Type.TRABALHO,
-      enums.Status.ATIVO,
-      enums.PhoneType.CELULAR,
-      4
-    ),
-    new Contact(
-      'Policia Militar',
-      'policia@gov.br',
-      '190',
-      enums.Type.EMERGENCIA,
-      enums.Status.ATIVO,
-      enums.PhoneType.FIXO,
-      5
-    ),
-    new Contact(
-      'Marcos Paulo',
-      'marcospaulo@hotmail.com',
-      '(41) 99999-9999',
-      enums.Type.AMIGOS,
-      enums.Status.ATIVO,
-      enums.PhoneType.CELULAR,
-      6
-    ),
-    new Contact(
-      'Jônattas Moraes',
-      'jonatas.moraes.09@hotmail.com',
-      '(41) 99999-9999',
-      enums.Type.FAMILIA,
-      enums.Status.ATIVO,
-      enums.PhoneType.CELULAR,
-      7
-    ),
-    new Contact(
-      'Ana Julia',
-      'anajulia@hotmail.com',
-      '(41) 99999-9999',
-      enums.Type.AMIGOS,
-      enums.Status.BLOQUEADO,
-      enums.PhoneType.CELULAR,
-      8
-    ),
-    new Contact(
-      'Tiago Teixeira',
-      'tiago100@hotmail.com',
-      '(41) 3999-9999',
-      enums.Type.FAMILIA,
-      enums.Status.ATIVO,
-      enums.PhoneType.FIXO,
-      9
-    ),
-    new Contact(
-      'Silvana teles',
-      'silvateles@hotmail.com',
-      '(41) 3699-9999',
-      enums.Type.TRABALHO,
-      enums.Status.BLOQUEADO,
-      enums.PhoneType.FIXO,
-      10
-    )
-  ],
+  initialState,
   reducers: {
     remove: (state, action: PayloadAction<number>) => {
-      return state.filter((contact) => contact.id !== action.payload)
+      state.items = [
+        ...state.items.filter((contact) => contact.id !== action.payload)
+      ]
+    },
+    edit: (state, action: PayloadAction<Contact>) => {
+      const contactIndex = state.items.findIndex(
+        (c) => c.id === action.payload.id
+      )
+      if (contactIndex >= 0) {
+        state.items[contactIndex] = action.payload
+      }
     }
   }
 })
 
-export const { remove } = contactSlice.actions
+export const { remove, edit } = contactSlice.actions
 
 export default contactSlice.reducer
